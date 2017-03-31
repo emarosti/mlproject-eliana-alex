b2d478486fc0008e7909b23cbc47fcd07f42a01c
Alex <thefamily.guo@gmail.com>
2017-03-31 15:03:08 -0400

    added comments and cleaned up unnecessary lines

--- data_loading.py
+++ data_loading.py
@@ -1,5 +1,4 @@
 import sys
-import csv
 import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
 
@@ -15,24 +14,26 @@ def load(filename):
     fullY = data[:, data.shape[1]-4:] # hard-coded for number of class columns
     return (X, fullY)
 
-def split_sets(X, fullY):
-    """
+def split_sets(X, fullY, test_size=0.2):
+    """given two matrices of data and associated labels,
+    split the dataset into a training and testing set (of proportion test_size,
+    default to 20%)
     """
     data = X
     labels = fullY[:,-1] # last column of 8 classes (0-7)
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=0)
     for train_indices, test_indices in sss.split(data, labels): # split returns generator type
         split_files(train_indices, data, fullY, "data/training.csv") # hard-coded because n_split = 1
         split_files(test_indices, data, fullY, "data/testing.csv") # hard-coded because n_split = 1
 
 def split_files(index_list, data, labels, outfile):
-    """
+    """HELPER FUNCTION
+    saves selected indices of provided data and labels into a .csv file
     """
     data_out = []
     data_csv = open(outfile, 'w')
     for i in index_list:
         entry = data[i,:]
-        print type(entry), entry.shape
         entry = np.append(entry, labels[i,:])
         data_out.append(entry)
     np.savetxt(outfile, data_out, fmt='%1.6f', delimiter=',', newline='\n') # labels are also printed as floats
